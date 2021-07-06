1b93798bf146ccbef16ab3ce23494359f2044e8f
ReimuWang <1505580759@qq.com>
2021-07-06 23:39:04 +0800

    init

---
+++ src/main/java/com/reimuwang/jellymushroom/JellyMushroomApplication.java
@@ -0,0 +1,13 @@
+package com.reimuwang.jellymushroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyMushroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellyMushroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reimuwang/jellymushroom/JellyMushroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reimuwang.jellymushroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyMushroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

68307512757519bb3a2ad7b7676c81baf87756e3
ReimuWang <1505580759@qq.com>
2021-07-07 00:08:34 +0800

    初始化应用

---
+++ src/main/java/team/jellymushroom/jmgame/JellyMushroomGameApplication.java
@@ -0,0 +1,13 @@
+package team.jellymushroom.jmgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyMushroomGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellyMushroomGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/jellymushroom/jmgame/JellyMushroomGameApplicationTests.java
@@ -0,0 +1,13 @@
+package team.jellymushroom.jmgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyMushroomGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

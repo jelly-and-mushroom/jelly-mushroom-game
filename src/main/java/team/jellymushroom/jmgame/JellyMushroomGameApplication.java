d9f983b47b350888ee46b092ee7ad2ba0758815f
ReimuWang <1505580759@qq.com>
2021-07-07 15:31:31 +0800

    添加日志模块

--- src/main/java/team/jellymushroom/jmgame/JellyMushroomGameApplication.java
+++ src/main/java/team/jellymushroom/jmgame/JellyMushroomGameApplication.java
@@ -3,6 +3,10 @@ package team.jellymushroom.jmgame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ * 除了springboot项目初始化得到的启动代码外，不添加额外的逻辑
+ */
 @SpringBootApplication
 public class JellyMushroomGameApplication {
 
---
+++ src/main/java/team/jellymushroom/jmgame/server/socket/SocketServer.java
@@ -0,0 +1,18 @@
+package team.jellymushroom.jmgame.server.socket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * socket通信服务端
+ */
+@Component
+@Slf4j
+public class SocketServer {
+
+  @PostConstruct
+  public void init() {
+  }
+}

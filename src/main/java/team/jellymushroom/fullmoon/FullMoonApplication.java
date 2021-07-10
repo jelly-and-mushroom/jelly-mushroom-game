6850675cf428940ef06d081e812b114d98de21b3
ReimuWang <1505580759@qq.com>
2021-07-10 11:14:49 +0800

    新建web应用

---
+++ src/main/java/team/jellymushroom/fullmoon/FullMoonApplication.java
@@ -0,0 +1,13 @@
+package team.jellymushroom.fullmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullMoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullMoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/jellymushroom/fullmoon/FullMoonApplicationTests.java
@@ -0,0 +1,13 @@
+package team.jellymushroom.fullmoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullMoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

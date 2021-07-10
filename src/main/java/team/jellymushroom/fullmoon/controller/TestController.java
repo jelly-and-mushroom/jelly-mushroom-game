5bafefe49252ecbb26282a6bec2dbe24857c872d
ReimuWang <1505580759@qq.com>
2021-07-10 11:41:10 +0800

    test web

---
+++ src/main/java/team/jellymushroom/fullmoon/controller/TestController.java
@@ -0,0 +1,17 @@
+package team.jellymushroom.fullmoon.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Slf4j
+public class TestController {
+
+  @GetMapping("/test")
+  @ResponseBody
+  public String getArticleList() {
+    return "测试成功";
+  }
+}

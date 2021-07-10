66f84933eff614a4554ef158a7286f3e1e245bfe
ReimuWang <1505580759@qq.com>
2021-07-10 10:59:38 +0800

    删除旧版本

--- jm-game-core/src/main/java/team/jellymushroom/jmgame/core/entity/game/state/StateEntity.java
+++
@@ -1,8 +0,0 @@
-package team.jellymushroom.jmgame.core.entity.game.state;
-
-/**
- * 玩家可获得的状态
- * 玩家每获得一个状态,都是新创建一个本类的实例
- */
-public abstract class StateEntity {
-}
--- start_client_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd jm-game-client/target
-java -jar jm-game-client-0.0.1-SNAPSHOT.jar

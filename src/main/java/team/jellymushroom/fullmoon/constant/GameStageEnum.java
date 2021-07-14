5e8b7150cb14410fd616d634f4e0cc689f2898cf
ReimuWang <1505580759@qq.com>
2021-07-14 20:09:49 +0800

    代码优化

--- src/main/java/team/jellymushroom/fullmoon/constant/GameStageEnum.java
+++ src/main/java/team/jellymushroom/fullmoon/constant/GameStageEnum.java
@@ -5,13 +5,11 @@ import lombok.Getter;
 @Getter
 public enum GameStageEnum {
 
-  // 角色选择
   CHOOSE_ROLE ("角色选择"),
   CHOOSE_ROLE_DETAIL ("角色选择-详情"),
   CHOOSE_ROLE_CONFIRM("角色选择-已确认");
 
   private String name;
-
 
   GameStageEnum(String name) {
     this.name = name;

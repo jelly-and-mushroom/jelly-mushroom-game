7d04f244f2ae483267d6d74e8504bedfa2006875
ReimuWang <1505580759@qq.com>
2021-07-16 15:20:08 +0800

    代码优化

--- src/main/java/team/jellymushroom/fullmoon/service/ChooseRoleService.java
+++ src/main/java/team/jellymushroom/fullmoon/service/ChooseRoleService.java
@@ -18,20 +18,21 @@ public class ChooseRoleService {
   }
 
   public void updateRole(int delta) {
-    if (null != this.mainService.getPlayerMyself().getGameRoleEntity()) {
-      return;
-    }
+    // 所有可选职业
     Map<Integer, GameRoleEntity> gameRoleMap = this.resourceService.getServiceResourceEntity().getGameRoleMap();
-    GameRoleEntity currentRole = this.mainService.getServerControlEntity().getCurrentChooseRole();
-    int preResult = currentRole.getIndex() + delta;
+    // 变更后的角色值
+    int preResult = this.mainService.getServerControlEntity().getCurrentChooseRole().getIndex() + delta;
+    // 小于下限则设为上限
     if (preResult < 0) {
       this.mainService.getServerControlEntity().setCurrentChooseRole(gameRoleMap.get(gameRoleMap.size() - 1));
       return;
     }
+    // 大于上限则设为下限
     if (preResult >= gameRoleMap.size()) {
       this.mainService.getServerControlEntity().setCurrentChooseRole(gameRoleMap.get(0));
       return;
     }
+    // 设置为变更后的值
     this.mainService.getServerControlEntity().setCurrentChooseRole(gameRoleMap.get(preResult));
   }
 

eac61350ed7a9eb31e434f0129bf69ee3c9b05e4
ReimuWang <1505580759@qq.com>
2021-07-24 21:27:15 +0800

    代码名称调整

--- src/main/java/team/jellymushroom/fullmoon/ui/module/PrepareOptionModule.java
+++ src/main/java/team/jellymushroom/fullmoon/ui/module/PrepareOptionModule.java
@@ -9,17 +9,17 @@ import java.awt.*;
 
 public class PrepareOptionModule extends Module {
 
-  private PrepareOptionEnum prepareOptionEnum;
+  private PrepareOptionEnum prepareOption;
 
-  public PrepareOptionModule(UIService uiService, UIResourceEntity resource, int oX, int oY, int oWidth, int oHeight, int padding, PrepareOptionEnum prepareOptionEnum) {
+  public PrepareOptionModule(UIService uiService, UIResourceEntity resource, int oX, int oY, int oWidth, int oHeight, int padding, PrepareOptionEnum prepareOption) {
     super(uiService, resource, oX, oY, oWidth, oHeight, padding);
-    this.prepareOptionEnum = prepareOptionEnum;
+    this.prepareOption = prepareOption;
   }
 
   @Override
   public void draw(Graphics g) {
     // 绘制底色
-    Color rectColor = this.prepareOptionEnum.equals(ServerControlEntity.getInstance().getCurrentPrepare()) ? Color.GRAY : Color.DARK_GRAY;
+    Color rectColor = this.prepareOption.equals(ServerControlEntity.getInstance().getCurrentPrepare()) ? Color.GRAY : Color.DARK_GRAY;
     super.drawFillRect(g, this.iX, this.iY, this.iWidth, this.iHeight, rectColor);
     // 绘制文字
     this.drawOptionFont(g);
@@ -31,34 +31,34 @@ public class PrepareOptionModule extends Module {
     int yAdd = 43;
     int fontSize = 20;
     int fontStyle = Font.PLAIN;
-    if (PrepareOptionEnum.DONE.equals(this.prepareOptionEnum)) {
-      super.drawFont(g, this.iX + 300, this.iY + yAdd, this.prepareOptionEnum.getDescription(), Color.WHITE, fontStyle, fontSize);
+    if (PrepareOptionEnum.DONE.equals(this.prepareOption)) {
+      super.drawFont(g, this.iX + 300, this.iY + yAdd, this.prepareOption.getDescription(), Color.WHITE, fontStyle, fontSize);
       return;
     }
-    if (this.prepareOptionEnum.getValue() == -1) {
-      super.drawFont(g, this.iX + 125, this.iY + yAdd, this.prepareOptionEnum.getDescription(), Color.WHITE, fontStyle, fontSize);
+    if (this.prepareOption.getValue() == -1) {
+      super.drawFont(g, this.iX + 125, this.iY + yAdd, this.prepareOption.getDescription(), Color.WHITE, fontStyle, fontSize);
       return;
     }
-    if (this.prepareOptionEnum.getLimitValue() == -1) {
-      super.drawFont(g, this.iX + 25, this.iY + yAdd, this.prepareOptionEnum.getDescription() + this.prepareOptionEnum.getValue() , Color.WHITE, fontStyle, fontSize);
-      super.drawFont(g, this.iX + 165, this.iY + yAdd, "(" + this.prepareOptionEnum.getPrice() + ")" , Color.ORANGE, fontStyle, fontSize);
+    if (this.prepareOption.getLimitValue() == -1) {
+      super.drawFont(g, this.iX + 25, this.iY + yAdd, this.prepareOption.getDescription() + this.prepareOption.getValue() , Color.WHITE, fontStyle, fontSize);
+      super.drawFont(g, this.iX + 165, this.iY + yAdd, "(" + this.prepareOption.getPrice() + ")" , Color.ORANGE, fontStyle, fontSize);
       return;
     }
     int currentValue = this.getCurrentValue();
-    Color limitColor = currentValue<this.prepareOptionEnum.getLimitValue() ? Color.CYAN : Color.PINK;
-    super.drawFont(g, this.iX + 10, this.iY + yAdd, this.prepareOptionEnum.getDescription() + this.prepareOptionEnum.getValue(), Color.WHITE, fontStyle, fontSize);
-    super.drawFont(g, this.iX + 120, this.iY + yAdd, "[" + currentValue + "/" + this.prepareOptionEnum.getLimitValue() + "]", limitColor, fontStyle, fontSize);
-    super.drawFont(g, this.iX + 170, this.iY + yAdd, "(" + this.prepareOptionEnum.getPrice() + ")", Color.ORANGE, fontStyle, fontSize);
+    Color limitColor = currentValue<this.prepareOption.getLimitValue() ? Color.CYAN : Color.PINK;
+    super.drawFont(g, this.iX + 10, this.iY + yAdd, this.prepareOption.getDescription() + this.prepareOption.getValue(), Color.WHITE, fontStyle, fontSize);
+    super.drawFont(g, this.iX + 120, this.iY + yAdd, "[" + currentValue + "/" + this.prepareOption.getLimitValue() + "]", limitColor, fontStyle, fontSize);
+    super.drawFont(g, this.iX + 170, this.iY + yAdd, "(" + this.prepareOption.getPrice() + ")", Color.ORANGE, fontStyle, fontSize);
   }
 
   private int getCurrentValue() {
-    if (PrepareOptionEnum.PROMOTE_INIT_HAND_CARD.equals(this.prepareOptionEnum)) {
+    if (PrepareOptionEnum.PROMOTE_INIT_HAND_CARD.equals(this.prepareOption)) {
       return this.uiService.getMainService().getPlayerMyself().getInitHandCardSize();
     }
-    if (PrepareOptionEnum.PROMOTE_MAX_HAND_CARD_SIZE.equals(this.prepareOptionEnum)) {
+    if (PrepareOptionEnum.PROMOTE_MAX_HAND_CARD_SIZE.equals(this.prepareOption)) {
       return this.uiService.getMainService().getPlayerMyself().getMaxHandCardSize();
     }
-    if (PrepareOptionEnum.PROMOTE_DRAW_CARD_SIZE.equals(this.prepareOptionEnum)) {
+    if (PrepareOptionEnum.PROMOTE_DRAW_CARD_SIZE.equals(this.prepareOption)) {
       return this.uiService.getMainService().getPlayerMyself().getDrawCardSize();
     }
     return -1;

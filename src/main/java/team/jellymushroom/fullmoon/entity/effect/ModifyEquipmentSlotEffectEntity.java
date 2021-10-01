3ca648c1989895b7fe9e244a48a36bfc34422abc
ReimuWang <1505580759@qq.com>
2021-10-01 19:06:46 +0800

    新增祝福-有备而来

---
+++ src/main/java/team/jellymushroom/fullmoon/entity/effect/ModifyEquipmentSlotEffectEntity.java
@@ -0,0 +1,20 @@
+package team.jellymushroom.fullmoon.entity.effect;
+
+import lombok.Data;
+import team.jellymushroom.fullmoon.entity.game.PlayerEntity;
+
+@Data
+public class ModifyEquipmentSlotEffectEntity extends EffectEntity {
+
+  private Integer modifyValue;
+
+  @Override
+  public void execute(PlayerEntity ownerPlayer, PlayerEntity opponentPlayer) {
+    PlayerEntity player = this.owner ? ownerPlayer : opponentPlayer;
+    int wish = player.getInitEquipmentSlotSize() + this.modifyValue;
+    if (wish < 0) {
+      wish = 0;
+    }
+    player.setInitEquipmentSlotSize(wish);
+  }
+}
